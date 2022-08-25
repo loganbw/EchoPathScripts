2ab21d79bcb6eac137b789cef12698fa11808379
Logan Weathersby <loganbryantweathersby@gmail.com>
2022-08-25 19:22:00 -0400

    pushing comment fix

--- MothUIMotor.cs
+++ MothUIMotor.cs
@@ -15,20 +15,20 @@ public class MothUIMotor : MonoBehaviour
     }
     void Move()
     {
-        /** Move this ship forward per frame, if it gets too far from the game area, destroy it **/
+        /** Move this moth forward per frame, if it gets too far from the game area, destroy it **/
 
         transform.position += transform.up * (Time.deltaTime * speed);
 
         float distance = Vector3.Distance(transform.position, levelArea.transform.position);
         if (distance > rmoths.deathSpawnCircleRadius)
         {
-            RemoveShip();
+            Removemoth();
         }
     }
 
-    void RemoveShip()
+    void Removemoth()
     {
-        /** Update the total ship count and then destroy this individual ship. **/
+        /** Update the total moth count and then destroy this individual moth. **/
 
         Destroy(gameObject);
         rmoths.mothCount -= 1;
